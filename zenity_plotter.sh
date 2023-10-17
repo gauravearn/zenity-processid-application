24969a67f5582b8b814417deefae8040999efb02
Gaurav Sablok <sablokg@gmail.com>
2023-10-18 00:04:58 +0530

    Add files via upload

---
+++ zenity_plotter.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# -*- coding:  utf-8 -*-
+# Author: Gaurav Sablok
+# date: 2023-09-15
+# MIT License
+# a zenity way of plotting the docker network and for killing the processids
+docker_address=$(zenity --forms --width=300 --height=100 \
+                                                --title "plot your ip address" \
+                                                                --add-entry="docker_address")
+IP="$docker_address"
+if [[ -z "IP" ]]
+then
+    echo "there is no network"
+    exit 1
+else
+    ping "$IP" > "${IP}".log && cat "${IP}".log | \
+                             cut -f 7 -d " " | \
+                                cut -f 2 -d "=" | uplot bar -d, -t "time latency"
+kill "${IP}"
+fi
